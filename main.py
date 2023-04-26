b7809dff23c260a5bb2049a98824346f54bbde4b
Carreri95 <117058725+Carreri95@users.noreply.github.com>
2023-04-26 22:34:41 +0000

    It divide and subtract numbers operations

--- main.py
+++ main.py
@@ -2,7 +2,11 @@
 
 from first_code.hello_world import printHelloWorld
 from operators.operators import add
-
+from operators.operators import subtraction
+from operators.operators import division
+from operators.operators import 
 printHelloWorld()
 
 print(add(1.5, 1.6))
+print(subtraction(2, 1))
+print(division(10, 2))
--- operators/operators.py
+++ operators/operators.py
@@ -13,3 +13,30 @@ def add(a, b):
   if type(a) == str or type(b) == str:
     raise TypeError()
   return a + b
+
+
+def subtraction(a, b):
+  """It subtract two numbers
+     parameter a: First value
+     type a: int, float
+     parameter b: Second value
+     type b: int, float
+
+     return: int, float
+  """
+  if type(a) == str or type(b) == str:
+    raise TypeError()
+  return a - b
+
+def division(a, b):
+  """It division two numbers
+     parameter a: First value
+     type a: int, float
+     parameter b: Second value
+     type b: int, float
+
+     return: int, float
+  """
+  if type(a) == str or type(b) == str:
+    raise TypeError()
+  return a / b
